379e517933c0a831edf69582b8a6e033a735bedb
krispagkaliwangan <krispagkaliwangan@gmail.com>
2018-05-17 19:31:49 +0800

    modified gitignore, and try to login

--- src/main/java/ph/krisp/stocks/App.java
+++ src/main/java/ph/krisp/stocks/App.java
@@ -2,6 +2,7 @@ package ph.krisp.stocks;
 
 import java.io.IOException;
 
+import org.jsoup.Connection.Response;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -15,6 +16,29 @@ public class App
 {
     public static void main( String[] args )
     {
+    	login();
+        
+    }
+    
+    private static void login() {
+    	try {
+            
+            //grab login form page first
+            Response loginPageResponse = 
+                    Jsoup.connect("https://www.investagrams.com/Welcome/Login")
+                    .userAgent("Mozilla/5.0")
+                    .timeout(10 * 1000)
+                    .followRedirects(true)
+                    .execute();
+            
+            System.out.println("Fetched login page");
+    	} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+    
+    private static void test() {
         try {
 			Document doc = Jsoup.connect("https://www.investagrams.com/Stock/SMPH")
 								.userAgent("Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36")
@@ -35,6 +59,5 @@ public class App
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-        
     }
 }
