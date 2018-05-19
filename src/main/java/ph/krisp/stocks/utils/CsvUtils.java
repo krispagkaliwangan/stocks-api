e56449280c131309cf9a31f61b79d6d60de84f91
krispagkaliwangan <krispagkaliwangan@gmail.com>
2018-05-19 22:24:40 +0800

    S-4 #comment added writer/appender to csv

--- src/main/java/ph/krisp/stocks/utils/CsvUtils.java
+++ src/main/java/ph/krisp/stocks/utils/CsvUtils.java
@@ -1,5 +1,13 @@
 package ph.krisp.stocks.utils;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+
 import ph.krisp.stocks.model.Stock;
 
 /**
@@ -12,7 +20,30 @@ public class CsvUtils {
 
 	private CsvUtils() {}
 	
+	/**
+	 * Appends the stock data to the existing file. Creates a new file if not
+	 * found
+	 * 
+	 * @param stock
+	 */
 	public static void updateStockFile(Stock stock) {
+		
+		String dir = WebUtils.getOutputPath();
+		String file = dir + stock.getCode() + ".csv";
+		
+		System.out.println(file);
+		
+
+		try (
+			BufferedWriter writer = new BufferedWriter(new FileWriter(file, true));
+			CSVPrinter csvPrinter = new CSVPrinter(writer, CSVFormat.DEFAULT); ) {
+
+			csvPrinter.printRecord(stock.getCode(), stock.getClose());
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 		
 	}
 	
---
+++ src/test/java/ph/krisp/stocks/utils/CsvUtilsTest.java
@@ -0,0 +1,32 @@
+package ph.krisp.stocks.utils;
+
+import static org.junit.Assert.*;
+
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import ph.krisp.stocks.connection.WebCon;
+import ph.krisp.stocks.model.Stock;
+
+public class CsvUtilsTest {
+
+	private Map<String, String> cookies;
+	private Map<String, Stock> stockInfo;
+	
+	@Before
+	public void setUp() throws Exception {
+		cookies = WebCon.login();
+    	stockInfo = WebCon.getAllStockInfo(cookies);
+	}
+
+	@Test
+	public void test() {
+		
+		for(Stock s : stockInfo.values()) {
+			CsvUtils.updateStockFile(s);
+		}
+	}
+
+}
