239db23c80d7f60b01c57e45ada3f2ef96940f63
krispagkaliwangan <krispagkaliwangan@gmail.com>
2018-05-26 19:49:13 +0800

    s-10 #comment added Stock class for analysis

---
+++ src/main/java/ph/krisp/stocks/model/Stock.java
@@ -0,0 +1,12 @@
+package ph.krisp.stocks.model;
+
+/**
+ * Class containing the stock information ready for analysis
+ * 
+ * @author kris.pagkaliwangan
+ *
+ */
+public class Stock {
+
+	private String code;
+}
