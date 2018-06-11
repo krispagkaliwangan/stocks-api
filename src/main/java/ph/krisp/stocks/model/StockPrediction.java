65b5d74d1a29bc2835bd333d1d79a2026a74bc05
krispagkaliwangan <krispagkaliwangan@gmail.com>
2018-06-11 22:34:32 +0800

    S-23 #comment completed prediction class

--- src/main/java/ph/krisp/stocks/model/StockPrediction.java
+++ src/main/java/ph/krisp/stocks/model/StockPrediction.java
@@ -1,5 +1,7 @@
 package ph.krisp.stocks.model;
 
+import java.util.Date;
+
 /**
  * Class containing the prediction for a stock
  * 
@@ -8,4 +10,62 @@ package ph.krisp.stocks.model;
  */
 public class StockPrediction {
 
+	private String code;
+	private Date date;
+	private String action;
+	private String nextDay;
+	private boolean isMatch;
+	
+	/**
+	 * Constructor for making a prediction
+	 * 
+	 * @param code
+	 * @param date
+	 * @param action
+	 */
+	public StockPrediction(String code, Date date, String action) {
+		this.code = code;
+		this.date = date;
+		this.action = action;
+	}
+
+	/**
+	 * Constructor for comparing a prediction to the actual next day
+	 * 
+	 * @param code
+	 * @param date
+	 * @param action
+	 * @param nextDay
+	 */
+	public StockPrediction(String code, Date date, String action,
+			String nextDay) {
+		this.code = code;
+		this.date = date;
+		this.action = action;
+		this.nextDay = nextDay;
+		this.isMatch = action.equals(nextDay);
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public String getAction() {
+		return action;
+	}
+
+	public String getNextDay() {
+		return nextDay;
+	}
+
+	public boolean isMatch() {
+		return isMatch;
+	}
+	
+	
+	
 }
